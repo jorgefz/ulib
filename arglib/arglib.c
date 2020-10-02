f28b6a8c09ac342c217deab932d27ce63cf6c19a
jorgefz <jorgefz.fernandez@gmail.com>
2020-10-02 14:13:48 +0100

    Description string is now dynamically allocated. _ARGS struct is now hidden from users

--- arglib/arglib.c
+++ arglib/arglib.c
@@ -9,13 +9,16 @@
 
 
 /* Data type string names corresponding to data type macro values */
-static char *_dtypes[] = {"flag", "int", "float", "string"};
+static char *_dtypes[] = {"flag", "int", "double", "string"};
 
 /* String representation for required/optional flags */
 static char *_reqflag[] = {"(required)", "(optional)"};
 
 /* Length of argument list, global */
 size_t ARG_NUM = 0;
+
+/* Global pointer to args */
+static _ARGS *ARGLIB_ARG_STRUCT = NULL;
 
 
 /*
@@ -30,18 +33,14 @@ int arglib_argnum()
 Adds a new argument to the argument list with the input parameters.
 If the parameter values are out of bounds, NULL is returned.
 */
-_ARGS *arglib_add_option(_ARGS *args,
-							const char c_label,
-							const char *s_label, 
-							const char *descr,
-							int type, int req)
+_ARGS *arglib_add_option(const char c_label,
+						 const char *s_label, 
+						 const char *descr,
+						 int type, int req)
 {
 	
 	// Check if input parameters are out of bounds
 	if(s_label && strlen(s_label)+1 > (size_t)ARG_MAX_SLABEL_LEN)
-		return NULL;
-
-	if(descr && strlen(descr)+1 > (size_t)ARG_MAX_DESC_LEN)
 		return NULL;
 
 	if(type < ARG_FLAG || type > ARG_STR)
@@ -53,31 +52,43 @@ _ARGS *arglib_add_option(_ARGS *args,
 	//Check that labels are unique
 	for(size_t i=0; i<ARG_NUM; i++)
 	{
-		if(s_label && strcmp(s_label,args[i].s_label)==0){
+		if(s_label && strcmp(s_label,ARGLIB_ARG_STRUCT[i].s_label)==0){
 			return NULL;
 		}
-		if(c_label==args[i].c_label){
+		if(c_label==ARGLIB_ARG_STRUCT[i].c_label){
 			return NULL;
 		}
 	}
 
 	// Increase list size
 	ARG_NUM++;
-	args = realloc(args, sizeof(_ARGS) * ARG_NUM );
+	ARGLIB_ARG_STRUCT = realloc(ARGLIB_ARG_STRUCT, sizeof(_ARGS) * ARG_NUM );
 
 	// Construct new argument element
 	_ARGS new_arg = {.c_label = c_label, .dtype = type, .flag_req = req};
 	
-	if(s_label){ strcpy(new_arg.s_label, s_label);}
-	else{ strcpy(new_arg.s_label, "\0");}
+	// Store string label
+	if(s_label){
+		strcpy(new_arg.s_label, s_label);
+	}
+	else{
+		strcpy(new_arg.s_label, "\0");
+	}
 
-	if(descr){ strcpy(new_arg.descr, descr);}
-	else{ strcpy(new_arg.descr, "\0");}
+	// Store description
+	if(descr){
+		new_arg.descr = malloc(strlen(descr)+1);
+		strcpy(new_arg.descr, descr);
+	}
+	else{
+		new_arg.descr = malloc(2);
+		strcpy(new_arg.descr, "\0");
+	}
 
 	// Insert into argument list
-	memcpy(args+(ARG_NUM-1), &new_arg, sizeof(_ARGS));
+	memcpy(&ARGLIB_ARG_STRUCT[ARG_NUM-1], &new_arg, sizeof(_ARGS));
 
-	return args;
+	return ARGLIB_ARG_STRUCT;
 }
 
 /*
@@ -85,31 +96,32 @@ Initialises option list and adds the help flag
 */
 _ARGS *arglib_init()
 {
-	_ARGS *args = NULL;
 	ARG_NUM = 0;
 
 	//Initialise with first command: help
-	args = arglib_add_option(args, 'h', "help", "Displays valid arguments and exits the program", ARG_FLAG, ARG_OPT);
+	ARGLIB_ARG_STRUCT = arglib_add_option('h', "help", "Displays valid arguments and exits the program", ARG_FLAG, ARG_OPT);
 
-	return args;
+	return ARGLIB_ARG_STRUCT;
 }
 
 /*
 Frees option argument list
 */
-void arglib_free(_ARGS *args)
+void arglib_free()
 {
 	for(size_t i=0; i<ARG_NUM; i++){
-		free(args[i].pval);
+		free(ARGLIB_ARG_STRUCT[i].descr);
+		free(ARGLIB_ARG_STRUCT[i].pval);
 	}
-	free(args);
+	free(ARGLIB_ARG_STRUCT);
+	ARGLIB_ARG_STRUCT = NULL;
 	ARG_NUM = 0;
 }
 
 /*
 Displays information of all predefined options.
 */
-static void arglib_help(_ARGS *args, const char *argv[])
+static void arglib_help(const char *argv[])
 {
 	printf(" USAGE: %s --[LABEL] [VALUE] --[FLAG]\n", argv[0]);
 	printf("        %s -[LABEL] [VALUE] -[FLAG]\n", argv[0]);
@@ -117,15 +129,15 @@ static void arglib_help(_ARGS *args, const char *argv[])
 	for(size_t i=0; i<ARG_NUM; i++)
 	{
 		// Char Label
-		printf(" %5s-%c,", "", args[i].c_label);
+		printf(" %5s-%c,", "", ARGLIB_ARG_STRUCT[i].c_label);
 		// Multi-char label
-		printf(" %5s--%s ", "", args[i].s_label);
+		printf(" %5s--%s ", "", ARGLIB_ARG_STRUCT[i].s_label);
 
 		// Param type & Req
-		printf(": %10s(%s)%s", "", _dtypes[args[i].dtype], _reqflag[args[i].flag_req]);
+		printf(": %10s(%s)%s", "", _dtypes[ARGLIB_ARG_STRUCT[i].dtype], _reqflag[ARGLIB_ARG_STRUCT[i].flag_req]);
 
 		// Description
-		printf(" %s\n", args[i].descr);
+		printf(" %s\n", ARGLIB_ARG_STRUCT[i].descr);
 	}
 }
 
@@ -255,14 +267,14 @@ static _ARGS *arglib_cast_arg(_ARGS *arg, const char *argv)
 Compares input arguments with defined argument list
 and extracts values. 
 */
-_ARGS *arglib_parse(_ARGS *args, const int argc, const char *argv[])
+_ARGS *arglib_parse(const int argc, const char *argv[])
 {
 
 	// Look for HELP command
 	for(size_t i=1; i<(size_t)argc; i++)
 	{
 		if( strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0 ){
-			arglib_help(args, argv);
+			arglib_help(argv);
 			return NULL;
 		}
 	}
@@ -281,27 +293,27 @@ _ARGS *arglib_parse(_ARGS *args, const int argc, const char *argv[])
 		short found = 0;
 		for(size_t j=1; j<ARG_NUM; j++)
 		{
-			if(arglib_cmp_arg(&args[j], argv[i])){
+			if(arglib_cmp_arg(&ARGLIB_ARG_STRUCT[j], argv[i])){
 				found = 1;
 				//Check if value already read
-				if(args[j].pval){
+				if(ARGLIB_ARG_STRUCT[j].pval){
 					fprintf(stderr, " [ARGLIB] Error: argument '%s' already defined\n", argv[i]);
 					return NULL;
 				}
 				//Seek parameter for non-flag arg
-				if(args[j].dtype != ARG_FLAG){
+				if(ARGLIB_ARG_STRUCT[j].dtype != ARG_FLAG){
 					//Catch parameter following label
-					if(i == (size_t)argc-1 || arglib_cast_arg(&args[j],argv[i+1]) == NULL){	
-						fprintf(stderr, " [ARGLIB] Error: argument '%s' requires %s parameter\n", argv[i], _dtypes[args[j].dtype]);
-						arglib_usage_info(&args[j]);
+					if(i == (size_t)argc-1 || arglib_cast_arg(&ARGLIB_ARG_STRUCT[j],argv[i+1]) == NULL){	
+						fprintf(stderr, " [ARGLIB] Error: argument '%s' requires %s parameter\n", argv[i], _dtypes[ARGLIB_ARG_STRUCT[j].dtype]);
+						arglib_usage_info(&ARGLIB_ARG_STRUCT[j]);
 						return NULL;
 					}
 					i++;
 				}
 				//Set defined flag value as '0'
 				else{
-					args[j].pval = malloc(sizeof(int));
-					*((int*)args[j].pval) = 0;
+					ARGLIB_ARG_STRUCT[j].pval = malloc(sizeof(int));
+					*((int*)ARGLIB_ARG_STRUCT[j].pval) = 0;
 				}
 				break;
 			}
@@ -315,14 +327,14 @@ _ARGS *arglib_parse(_ARGS *args, const int argc, const char *argv[])
 	//Check that all required arguments are filled
 	for(size_t i=0; i<ARG_NUM; i++)
 	{
-		if(args[i].flag_req == ARG_REQ && !args[i].pval){
-			fprintf(stderr, " [ARGLIB] Error: required argument '%s' not defined\n", args[i].s_label);
+		if(ARGLIB_ARG_STRUCT[i].flag_req == ARG_REQ && !ARGLIB_ARG_STRUCT[i].pval){
+			fprintf(stderr, " [ARGLIB] Error: required argument '%s' not defined\n", ARGLIB_ARG_STRUCT[i].s_label);
 			return NULL;
 		}
 	}
 
 
-	return args;
+	return ARGLIB_ARG_STRUCT;
 }
 
 
@@ -332,14 +344,14 @@ Retrieves the input argument value read from the parser.
 The value must be cast to te appropriate data type.
 A NULL value means no value was read.
 */
-void *arglib_get_value(_ARGS *args, const char *arg_label)
+void *arglib_get_value(const char *arg_label)
 {
 	for(size_t i=0; i<ARG_NUM; i++)
 	{
-		if( strlen(arg_label) > 1 && strcmp(args[i].s_label, arg_label) == 0)
-			return (args[i].pval);
-		if( strlen(arg_label) == 1 && args[i].c_label == arg_label[0])
-			return (args[i].pval);
+		if( strlen(arg_label) > 1 && strcmp(ARGLIB_ARG_STRUCT[i].s_label, arg_label) == 0)
+			return (ARGLIB_ARG_STRUCT[i].pval);
+		if( strlen(arg_label) == 1 && ARGLIB_ARG_STRUCT[i].c_label == arg_label[0])
+			return (ARGLIB_ARG_STRUCT[i].pval);
 	}
 	return NULL;
 }
--- arglib/readme.md
+++ arglib/readme.md
@@ -3,26 +3,26 @@
 
 This library provides tools to interpret input command line arguments and extract data from them.
 
-Command line arguments may contain a series of options, called with a single character label '-o' or a multi-character string label '--option'. Options may require an argument that follows its label (e.g. '--integer 42') Options that require no argument are called flags, and are similar to a boolean (false by default, and true when called).
+Command line arguments may contain a series of options, called with a single character label '-o' or a multi-character string label '--option'. Options may require an argument that follows its label (e.g. '--integer 42') Options that require no argument are called flags, and are similar to a boolean (false by default, and true when called) (e.g. '--flag').
 
 ## Initialise
-An option list is stored in the struct `_ARGS`.
+A custom option list is stored in the struct `_ARGS`.
 Initialise the options list with the following function:
 ```c
-_ARGS *opt = arglib_init();
+arglib_init();
 ```
 This function automatically adds the help flag option, with labels '-h' and '--help'.
 
 ## Add new options
 In order to add a custom option, use the function:
 ```c
-_ARGS *ret = arglib_add_option(_ARGS *opt, 
-				const char c_label, 
-				const char *s_label,  
-				const char *descr, 
-				int type, int req);
+arglib_add_option(
+		const char c_label, 
+		const char *s_label,  
+		const char *descr, 
+		int type, int req);
 ```
-Here, 'opt' is the pointer returned from the initialisation function, 'c_label' is the single character label, 's_label' is the multi-character string label (of max length 16), 'descr' is a string (of maximum length 50) that describes the option, 'type' is the data type of option, and 'req' describes whether the option is required or optional.
+Here, 'c_label' is the single character label, 's_label' is the multi-character string label (of max length 16), 'descr' is a string that describes the option, 'type' is the data type of option, and 'req' describes whether the option is required or optional.
 
 Both the type and the required flag are defined with library-provided macros:
 
@@ -42,45 +42,44 @@ If the labels are identical to previously defined options, the new option isn't 
 
 It is not necessary to provide a description, or both the string and character labels simultaneously (only one is necessary). Instead of a string, pass a NULL pointer, and instead of a character, pass the `\0` character (or `(char)0`).
 
+The 'help' option is added automatically. Called with '-h' or '--help', it will display the available options, with their labels, descriptions, data types, and required/optional tag.
+
 ### Examples
 ```c
-_ARGS *opt = arglib_init();
+arglib_init();
 
 // Adds an option that asks for the number of iterations on a simulation, with labels '-i' and '--iter', that expects an integer as an argument, and must be defined (is required).
-arglib_add_option(opt, 'i', "iter", "Number of iterations in the simulation", ARG_INT, ARG_REQ);
+arglib_add_option('i', "iter", "Number of iterations in the simulation", ARG_INT, ARG_REQ);
 
 // This option asks for the path and filename for output files, with labels '-o' and '--output', and expects a string argument. This option, however, is optional.
-arglib_add_option(opt, 'o', "output", "Output filename & path", ARG_STR, ARG_OPT);
+arglib_add_option('o', "output", "Output filename & path", ARG_STR, ARG_OPT);
 ```
 
 ### Number of options
 To retrieve the number of defined options so far, use the function:
 ```c
-int argnum = arglib_argnum(_ARGS *opt);
+int argnum = arglib_argnum();
 ```
 
 ## Parse arguments
 The next step is to parse the input arguments with the defined options, using the following function:
 ```c
-_ARGS *ret = arglib_parse(_ARGS *opt, 
-				const int argc, 
-				const char *argv[]);
+arglib_parse(int argc, const char *argv[]);
 ```
 This function will search for option labels on the input arguments and read their arguments where applicable. If an error occurs, the function returns NULL; this includes an unrecognised option, a missing option argument or of the wrong data type, an undefined required option, or if one of the options is the help flag ('-h' or '--help').
 
 ## Get Values
 Now, the option values can be retrieved. This is done with the function:
 ```c
-void *ret = arglib_get_value(_ARGS *opt, const char *arg_label);
+void *ret = arglib_get_value(const char *arg_label);
 ```
-Providing the option struct 'args' and the predefined label (either single char or string) 'arg_label', will retrieve a void pointer to the memory location where the value is stored. The return value must be cast to the appropriate data type. If the option was not given a value, a NULL pointer is returned.
+Providing the option's predefined label (either one, as a string), will retrieve a void pointer to the memory location where the value is stored. The return value must be cast to the appropriate data type. If the option was not given a value, a NULL pointer is returned.
 
 For flags, if a flag is called, then the return pointer points to a location in memory and will evaluate to true. Otherwise, NULL is returned.
-The value of a defined flag will point to a location in memory that contains an integer 0.
 
 ## Freeing option list
 The option list is freed with the following function:
 ```c
-void arglib_free(_ARGS *opt);
+void arglib_free();
 ```
-Note that the memory where the option values are stored will be freed. In order to keep the values, copy them to memory that will remain accessible.
+Note that the memory locations to which `arglib_get_value()` points will also be freed.
