650580ea22f7e9ae930d2fbf202b84a7335f6771
Jorge Fernandez <jorgefz.fernandez@gmail.com>
2021-03-18 17:16:04 +0000

    array.h v0.1

--- utils/array.h
+++ utils/array.h
@@ -15,16 +15,22 @@ Compiler: GCC
 
 VERSIONS
 
-v0.1
-	- Basic creation, freeing: array_new and array->free()
-	- Value initialziation: fill() and range()
+v0.1 - 18/03/2021
+	- Basics: array_new() and free()
+	- Value generators: fill(), range(), linspace(), from_c_array()
 	- Printing: print()
 	- Debug: __array_debug();
 	- Types supported: int and double.
+	- Get values (geti, getf) or a pointer (at).
+	- Set values (seti, setf)
+
+Planned
+	- Stats: max, min, sum, average, mean, median, etc.
+	- Operations: add, sub, mult, div, mod
+	- Sorting: selection, bubble, etc.
 
 
 */
-
 
 
 #ifndef ARRAY_H
@@ -35,6 +41,7 @@ v0.1
 #include <stdint.h>
 #include <stdarg.h>
 #include <math.h>
+#include <string.h>
 
 
 /*
@@ -51,16 +58,23 @@ struct __attribute__((__packed__)) __array_struct {
 	uint64_t size;
 	uint8_t type;
 	uint64_t bytes; //size in bytes of each member
+
 	//Function pointers here
 	void (*free)(struct __array_struct*);
 	void (*fill)(struct __array_struct*, ...);
 	void (*print)(struct __array_struct*);
 	void (*range)(struct __array_struct*, ...);
+	void (*linspace)(struct __array_struct*, ...);
+	int (*geti)(struct __array_struct*, uint64_t ind);
+	double (*getf)(struct __array_struct*, uint64_t ind);
+	void (*seti)(struct __array_struct*, uint64_t ind, int value);
+	void (*setf)(struct __array_struct*, uint64_t ind, double value);
+	char* (*at)(struct __array_struct*, uint64_t ind);
+	void (*from_c_array)(struct __array_struct*, const void* c_arr);
 };
 typedef struct __array_struct array;
 
-enum { INT, DOUBLE };
-
+enum { INT, DOUBLE }; //Supported types
 
 
 /*
@@ -73,10 +87,13 @@ static inline uint64_t __type_bytes(uint8_t type);
 
 static inline void __array_free(array* arr);
 static inline void __array_debug(array* arr);
-static inline int __array_getval_int(array* arr, uint64_t ind);
-static inline double __array_getval_db(array* arr, uint64_t ind);
-static inline int* __array_getptr_int(array* arr, uint64_t ind);
-static inline double* __array_getptr_db(array* arr, uint64_t ind);
+static inline void __array_from_c_array(array* arr, const void* c_arr);   
+
+static inline int     __array_getval_int(array* arr, uint64_t ind);
+static inline double  __array_getval_db(array* arr, uint64_t ind);
+static inline char*   __array_getptr(array* arr, uint64_t ind);
+static inline void    __array_setval_int(array* arr, uint64_t ind, int value);
+static inline void    __array_setval_db(array* arr, uint64_t ind, double value);
 
 static inline void __array_fill_int(array* arr, int value);
 static inline void __array_fill_db(array* arr, double value);
@@ -89,6 +106,10 @@ static inline void __array_print(array* arr);
 static inline void __array_fill_range_int(array* arr, int start, int end);
 static inline void __array_fill_range_db(array* arr, double start, double end);
 static inline void __array_fill_range(array* arr, ...);
+
+static inline void __array_fill_linspace_int(array* arr, int start, int step);
+static inline void __array_fill_linspace_db(array* arr, double start, double step);
+static inline void __array_fill_linspace(array* arr, ...);
 
 static inline array* array_new(uint64_t size, uint8_t type);
 
@@ -122,30 +143,34 @@ void __array_debug(array* arr){
 	printf(" -size: %d\n", (int)arr->size);
 	printf(" -type: %d\n", (int)arr->type);
 	printf(" -bytes: %d\n", (int)arr->bytes);
-	printf(" -data at 0x%p\n", arr->data);
+	printf(" -data: 0x%p to 0x%p\n", &arr->data[0], &arr->data[0]+arr->bytes*(arr->size+1)-1);
 	//Function pointers
 	printf(" -free_func at 0x%p\n", arr->free);
 	printf(" -fill_func at 0x%p\n", arr->fill);
 	printf(" -print_func at 0x%p\n", arr->print);
 	printf(" -range_func at 0x%p\n", arr->range);
-
 }
 
 int __array_getval_int(array* arr, uint64_t ind){
-	return *(int*)(arr->data + arr->bytes*ind);
+	return *(int*)(&arr->data[0] + arr->bytes*ind);
 }
 
 double __array_getval_db(array* arr, uint64_t ind){
-	return *(double*)(arr->data + arr->bytes*ind);
+	return *(double*)(&arr->data[0] + arr->bytes*ind);
 }
 
-int* __array_getptr_int(array* arr, uint64_t ind){
-	return (int*)(arr->data + arr->bytes*ind);
+char* __array_getptr(array* arr, uint64_t ind){
+	return (char*)(&arr->data[0] + arr->bytes*ind);
 }
 
-double* __array_getptr_db(array* arr, uint64_t ind){
-	return (double*)(arr->data + arr->bytes*ind);
+void __array_setval_int(array* arr, uint64_t ind, int value){
+	*(int*)(arr->data + arr->bytes*ind) = value;
 }
+
+void __array_setval_db(array* arr, uint64_t ind, double value){
+	*(double*)(arr->data + arr->bytes*ind) = value;
+}
+
 
 // ----------------------------------
 
@@ -239,9 +264,42 @@ void __array_fill_range(array* arr, ...){
 	}
     va_end(args);
 }
+//------------------------------------
 
-//void __array_fill_linspace
-//void __array_from_c_array()
+
+void __array_fill_linspace_int(array* arr, int start, int step){
+	for(uint i=0; i!=arr->size; ++i){
+		arr->seti(arr, i, start);
+		start += step;
+	}
+}
+
+void __array_fill_linspace_db(array* arr, double start, double step){
+	for(uint i=0; i!=arr->size; ++i){
+		arr->setf(arr, i, start);
+		start += step;
+	}
+}
+
+void __array_fill_linspace(array* arr, ...){
+	va_list args;
+	va_start(args, arr);
+	switch(arr->type){
+		case INT:
+			__array_fill_linspace_int(arr, va_arg(args,int), va_arg(args,int));
+			break;
+		case DOUBLE:
+			__array_fill_linspace_db(arr, va_arg(args,double), va_arg(args,double));
+			break;
+	}
+    va_end(args);
+}
+
+///-----------------------------------
+
+void __array_from_c_array(array* arr, const void* c_arr){
+	memcpy(arr->data, c_arr, arr->bytes*arr->size);
+}
 
 //------------------------------------
 
@@ -265,6 +323,13 @@ array* array_new(uint64_t size, uint8_t type){
 	arr->fill = &__array_fill;
 	arr->print = &__array_print;
 	arr->range = &__array_fill_range;
+	arr->linspace = &__array_fill_linspace;
+	arr->geti = &__array_getval_int;
+	arr->getf = &__array_getval_db;
+	arr->at = &__array_getptr;
+	arr->seti = &__array_setval_int;
+	arr->setf = &__array_setval_db;
+	arr->from_c_array = &__array_from_c_array;
 
 	return arr;
 }
--- utils/test.c
+++ utils/test.c
@@ -17,22 +17,36 @@ int main(){
 		printf("Error!\n");
 		return 1;
 	}
-	__array_debug(arr);
+	//__array_debug(arr);
 	arr->fill(arr,5);
 	arr->print(arr);
-	arr->range(arr,9,99);
+	arr->range(arr,10,0);
+	arr->print(arr);
+	int c_arr[] = {99, 88, 77, 66, 55, 44, 33, 22, 11, -11};
+	arr->from_c_array(arr, c_arr);
+	arr->print(arr);
+	arr->free(arr);
+
+	arr = array_new(100, INT);
+	if(!arr) return 1;
+	arr->linspace(arr, -500, -111);
 	arr->print(arr);
 	arr->free(arr);
 
 	// array of doubles
 	array* nums = array_new(6, DOUBLE);
 	if(!nums){
-		printf("Error!\n");
+		fprintf(stderr, "Error!\n");
 		return 1;
 	}
-	__array_debug(nums);
+	//__array_debug(nums);
 	nums->range(nums, 3.1415, -0.77);
 	nums->print(nums);
+
+	double sum = 0;
+	for(uint i=0; i!=nums->size; ++i)
+		sum += nums->getf(nums,i);
+	printf("Average: %.11g\n", sum/(double)nums->size);
 	nums->free(nums);
 
 	return 0;
